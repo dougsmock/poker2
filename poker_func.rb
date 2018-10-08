2b4b1c1445bf6394d9c01b1091728445872a7043
Your Name <you@example.com>
2018-10-08 08:53:30 -0400

    After Sunday night.

--- poker_func.rb
+++ poker_func.rb
@@ -10,16 +10,50 @@ end
 
 def whitehand(newcard_arr)
   whitecards_arr = []
-  whitecards_arr << newcard_arr.pop(5)
+  whitecards_arr = newcard_arr.pop(5)
+  black_avail = newcard_arr
+  return
+  black_avail
+  return
   whitecards_arr
 end
 
+def blackhand(black_avail)
+  blackcards_arr = black_avail.pop(5)
+  blackcards_arr
+end
+
+def convert_white(whitecards_arr)
+    w = 0
+    whitecards_arr.each do |w|
+        until whitecards_arr[w] == nil
+            whitecards_arr[w] += 7
+            w += 1
+        end
+end
+
+### test to see if this conversion to a card number works.
+
+
+#     counter = 0
+#     blackcards_arr.each do |b|
+#         whitecards_arr[b] += 7
+#         counter += 1
+#     end
+
+#     return
+#         whitecards_arr, blackcards_arr
+    
+# end
 
 
 
 
 
 
-#1 would be 2 of clubs, or 2 modulus 0
+
+
+
 #ADD 7 TO ALL cards, making 52 a 59.
+#18 would be 2 of clubs, or 2 modulus 0
 #59 would be 14 modulus 3, an Ace of Hearts.
--- poker_tdd.rb
+++ poker_tdd.rb
@@ -16,14 +16,12 @@ class Poker_test < Minitest::Test
 
 	def test_card_position
 		card_arr = [*1..52]
-		p "Deal from #{card_arr}."
 		assert_equal(11, card_arr[10])
 	end
 
 	def test_shuffling_cards_length
 		card_arr = [*1..52]
 		newcard_arr = card_arr.shuffle
-		p "Shuffled cards are #{newcard_arr}."
 		assert_equal(52, newcard_arr.length)
 	end
 
@@ -33,14 +31,19 @@ class Poker_test < Minitest::Test
 		assert_equal([48, 49, 50, 51, 52], x_array)
 	end
 
-	# def white_hand_test
-	# 	test_array = [1, 2, 3, 4, 5, 6]
-	# 	x_array = test_array.pop(5)
-	# 	assert_equal([2, 3, 4, 5, 6], x_array)
-	# end
+	def test_of_shuffle_array_both_hands
+		test_arr = [*1..52]
+		p "Deal from #{test_arr}."
+		x_arr = test_arr.shuffle
+		p "Shuffled cards are #{x_arr}."
+		white = x_arr.pop(5)
+		p "White's hand is #{white}."
+		black = x_arr.pop(5)
+		p "Black's hand is #{black}."
+	end
 
-
-
-
+	def test_if_plus_seven_worked_white
+		assert_equal(x
+	end
 
 end
